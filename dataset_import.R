913f47b6028add135155b27b9652b726af29df85
Ginevra <ginevracoal@gmail.com>
2018-06-23 10:55:10 +0200

    importing dataset

---
+++ dataset_import.R
@@ -0,0 +1,10 @@
+samplesubmission <- read.csv("dataset/sampleSubmission.csv")
+str(samplesubmission)
+
+library(data.table)
+
+train <- as.data.frame(fread("dataset/train.tsv"))
+test <- as.data.frame(fread("dataset/test.tsv"))
+
+str(train)
+str(test)
