82653dbbdfdfda34a19eb8c728302423222c2f74
doma <doma@doma-PC.Home>
2018-06-24 12:44:20 +0200

    complete amazon processing

---
+++ amazon_json_complete.R
@@ -0,0 +1,122 @@
+#json
+
+#set right environment, run only once!
+oldwd <- getwd()
+setwd("/home/doma/0dssc/smds/statMethodsProject/input_amazon/")
+
+#from here rerun how much you want
+library(ReadMe)
+library(rjson)
+json_file <- "../dataset/Musical_Instruments_5.json"
+json_data <- json_data <- rjson::fromJSON(file=json_file,simplify = FALSE,method = "R")
+
+library("jsonlite")
+json_data = stream_in(file("../dataset/Musical_Instruments_5.json",open="r"))
+
+library(tidyverse)
+reviews=json_data %>%
+  select(reviewText,overall,reviewTime) %>%
+  add_column(SentenceId=1:length(json_data$reviewText))
+
+#n <- 2000
+n = length(json_data$reviewerID)
+# create text files with proper filenames (numbers)
+unlink("../input_amazon/*")
+data=reviews
+apply(data[1:n,], 1, 
+      function(x) write.table(data.frame(x[1]), 
+                              file = gsub(" ","", paste("../input_amazon/",x[4],sep="")), row.names = FALSE, col.names = FALSE))
+
+
+
+# create control file with same naming conventions as rotten tomatoes
+training_perc=0.5
+data1 <- data[1:n,] %>% 
+  rename(Sentiment=overall)%>%
+  select(SentenceId, Sentiment) %>% 
+  rename(filename = SentenceId) %>% 
+  rename(truth = Sentiment) %>% 
+  add_column(trainingset = rbinom(n, 1, training_perc))
+
+head(data1)
+
+write.csv(data1, file = "../input_amazon/control.txt", row.names = FALSE)       
+
+
+#analisi con readme
+
+#step undergrad preprocessing
+
+
+output <- undergrad(sep = ',') 
+
+# list the elements
+length(output)
+str(output)
+
+
+# PREPROCESS FUNCTION
+
+# remove columns with variance 0
+preprocess <- preprocess(output)
+
+length(preprocess)
+str(preprocess)
+
+# we can notice that the function preprocess is removing 3 columns
+length(output$trainingset)
+length(output$testset)
+
+length(preprocess$trainingset)
+length(preprocess$testset)
+
+# why?
+# it computes variances for all columns representing a single word
+v2 <- apply(as.matrix(preprocess$trainingset[, 4:dim(preprocess$trainingset)[2]]), 
+            2, sd)
+v2
+
+# and removes all words with 0 variance, meaning words not 
+# comparing in any document
+rmc2 <- 3 + which(v2 == 0)
+rmc2
+
+# compute the proportion of text documents within each of 
+# the user-specified categories
+results <- readme(undergradlist=preprocess)
+str(results)
+
+################plotting
+library(tidyverse)
+valori_fit = results$est.CSMF
+valori_veri = results$true.CSMF
+#converto in dataframe
+valori_fit = as_tibble(as.list(valori_fit))
+valori_veri = as_tibble(as.list(valori_veri))
+valori_plot=gather(valori_fit,type)
+valori_plot_veri=gather(valori_veri,type)
+#typeof(results$est.CSMF)
+valori_plot_veri
+valori_plot
+valori_tot=inner_join(valori_plot,valori_plot_veri,by="type",suffix=c(".est",".real"))
+
+ggplot(data=valori_tot,aes(x=value.real,y=value.est,label = value.est))+
+  geom_point(aes(color=type))+
+  ggtitle(" Computed vs fitted values for Amazon music dataset")+
+  ggplot2::labs(
+  title = "Computed vs fitted values for Amazon music dataset",
+    subtitle = paste("total number of elements:",n,"of which training:",100*training_perc,"%"))+
+  # geom_smooth(method=lm)+
+  xlab("Real value")+
+  ylab("Computed value")+
+  theme_minimal()+
+  geom_abline(slope = 1,intercept = 0)
+
+
+############################
+
+
+# restore old working directory
+setwd(oldwd)
+
+
